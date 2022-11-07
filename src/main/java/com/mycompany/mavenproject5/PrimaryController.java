bef32aff945221904d13b3ff6671486c869a06e7
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2022-11-07 16:01:25 -0500

    probando

--- src/main/java/com/mycompany/mavenproject5/PrimaryController.java
+++ src/main/java/com/mycompany/mavenproject5/PrimaryController.java
@@ -8,6 +8,6 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
-        System.out.println("ez");
+        System.out.println("ezpzi");
     }
 }
